d317b3313d441352c441fa5f30e672a80747ea32
Alastair Coote <alastair.coote@gmail.com>
2017-05-06 20:16:46 -0400

    remove default development namespace

--- src/util/namespace-topic.ts
+++ src/util/namespace-topic.ts
@@ -1,14 +1,14 @@
 
-export function namespaceTopic(topicName:string):string {
-    return "_" + (process.env.NODE_ENV || 'development') + "_" + topicName
+export function namespaceTopic(topicName: string): string {
+    return "_" + (process.env.NODE_ENV) + "_" + topicName
 }
 
 interface TopicAndNamespace {
-    topicName:string;
-    environment:string;
+    topicName: string;
+    environment: string;
 }
 
-export function unnamespaceTopic(namespacedTopicName:string):TopicAndNamespace {
+export function unnamespaceTopic(namespacedTopicName: string): TopicAndNamespace {
     let split = namespacedTopicName.split("_");
     split.shift();
     let environment = split.shift();
