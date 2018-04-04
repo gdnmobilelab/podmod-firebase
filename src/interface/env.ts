338572394f8f09f88b9479f792095ab4a188a4d1
Alastair Coote <alastair.coote@nytimes.com>
2018-04-04 12:01:33 -0400

    add validator stuff

---
+++ src/interface/env.ts
@@ -0,0 +1,13 @@
+export interface EnvironmentVariables {
+  FIREBASE_AUTH_KEY: string;
+  FIREBASE_SENDER_ID: string;
+  DATABASE_URL: string;
+  USER_API_KEY: string;
+  ADMIN_API_KEY: string;
+  NODE_ENV: string;
+  FIREBASE_PRIVATE_KEY: string;
+  FIREBASE_CLIENT_EMAIL: string;
+  FCM_PROJECT: string;
+  ALLOWED_ORIGINS?: string;
+  SERVER_PORT?: string;
+}
