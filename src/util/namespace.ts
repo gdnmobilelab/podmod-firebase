a9bdd7fa37753f35fa8b8e8ee2a933afc123d4c9
Alastair Coote <alastair.coote@nytimes.com>
2018-04-11 16:32:25 -0400

    namespacing, test for get subscribed

---
+++ src/util/namespace.ts
@@ -0,0 +1,33 @@
+import Environment from "./env";
+
+// Since everything goes through the same Firebase account (it has to with the apps)
+// we namespace our topics according to the environment Pushkin is currently working in.
+
+// We use encodeURIComponent and = as separators to ensure that the regex won't ever get
+// tripped up by the topic/prefix strings - = is always encoded.
+const ENCODED_PREFIX = encodeURIComponent(Environment.TOPIC_PREFIX);
+const ENCODED_ENV = encodeURIComponent(Environment.NODE_ENV);
+
+export function namespaceTopic(topic: string) {
+  let encodedTopic = encodeURIComponent(topic);
+
+  return `=${ENCODED_PREFIX}=${ENCODED_ENV}=${encodedTopic}`;
+}
+
+const EXTRACT_REGEX = new RegExp(`^=${Environment.TOPIC_PREFIX}=(.+)=(.+)$`);
+
+export function extractNamespacedTopic(namespacedTopic: string) {
+  let regexResult = EXTRACT_REGEX.exec(namespacedTopic);
+
+  if (!regexResult || !regexResult[2]) {
+    throw new Error(
+      `Could not extract topic from namespaced key ${namespacedTopic}` +
+        ` (env: ${Environment.NODE_ENV}, prefix: ${Environment.TOPIC_PREFIX}`
+    );
+  }
+
+  return {
+    env: regexResult[1],
+    topic: regexResult[2]
+  };
+}
---
+++ test/endpoints/get-subcribed.ts
@@ -0,0 +1,58 @@
+import * as nock from "nock";
+import fetch from "node-fetch";
+import { expect } from "chai";
+import { createServer } from "../../src/index";
+import { namespaceTopic } from "../../src/util/namespace";
+import Environment from "../../src/util/env";
+
+describe("Get subscribed topics", () => {
+  let stop: () => void;
+
+  before(async () => {
+    stop = await createServer();
+  });
+
+  after(async () => {
+    nock.cleanAll();
+    await stop();
+  });
+
+  it("Should get subscribed topics for a user", async () => {
+    let topicsToReturn = {};
+    topicsToReturn[namespaceTopic("TEST_TOPIC")] = 1;
+    // insert one from a different env to make sure we filter
+    topicsToReturn[`=${Environment.TOPIC_PREFIX}=NOTTEST=TEST_TOPIC`] = 1;
+
+    let nocked = nock("https://iid.googleapis.com", {
+      reqheaders: {
+        authorization: `key=${process.env.FIREBASE_AUTH_KEY}`
+      }
+    })
+      .get("/iid/info/TEST_USER")
+      .query({ details: "true" })
+      .reply(
+        200,
+        JSON.stringify({
+          rel: {
+            topics: topicsToReturn
+          }
+        })
+      );
+
+    // Now finally send our actual test request
+    let res = await fetch("http://localhost:3000/registrations/TEST_USER/topics", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        authorization: process.env.USER_API_KEY
+      }
+    });
+
+    let json = await res.json();
+
+    expect(res.status).to.eq(200);
+    expect(json.length).to.eq(1);
+    expect(json[0]).to.eq("TEST_TOPIC");
+    nocked.done();
+  });
+});
