b28bc4fa21e00b7f7e3d9f125d38f1c4944859b7
Alastair Coote <alastair.coote@nytimes.com>
2018-05-11 12:13:35 -0400

    build script

--- src/security/key-check.ts
+++ src/security/key-check.ts
@@ -3,28 +3,31 @@ import { ForbiddenError, NotFoundError } from "restify-errors";
 import Environment from "../util/env";
 
 export enum ApiKeyType {
-  Admin,
-  User
+  Admin = "admin",
+  User = "user"
 }
 
 export function checkForKey(keyType: ApiKeyType): restify.RequestHandler {
+  if (
+    (Environment.ALLOW_ADMIN_OPERATIONS === "false" && keyType === ApiKeyType.Admin) ||
+    (Environment.ALLOW_USER_OPERATIONS === "false" && keyType === ApiKeyType.User)
+  ) {
+    // This shuts off access to admin or user features entirely. The prime reason being so that
+    // we can separate out instances between our adm and pub environments.
+    //
+    // We send a 404 because we want to consider this URL to simply not exist.
+
+    return function(req, res, next) {
+      req.log.warn({ url: req.url, environment: keyType }, "Attempt to access an endpoint in a disabled environment");
+      next(new NotFoundError());
+    };
+  }
+
   // Slightly confusing, but this is a function that returns a function. That way we can specify
   // the level of authorisation we want when setting up routes, but have the actual authorisation
   // function run when the request is received.
 
   return function(req, res, next) {
-    if (
-      (Environment.ALLOW_ADMIN_OPERATIONS === "false" && keyType === ApiKeyType.Admin) ||
-      (Environment.ALLOW_USER_OPERATIONS === "false" && keyType === ApiKeyType.User)
-    ) {
-      // This shuts off access to admin or user features entirely. The prime reason being so that
-      // we can separate out instances between our adm and pub environments.
-      //
-      // We send a 404 because we want to consider this URL to simply not exist.
-      next(new NotFoundError());
-      return;
-    }
-
     let auth = req.headers.authorization;
 
     if (!auth) {
