1f171f289fcbb4f9dd7e98f3d7bed67cd709b6b7
Alastair Coote <alastair.coote@nytimes.com>
2018-07-19 12:25:24 -0400

    add to subscription log tables as part of migrations

--- migrations/1531948410830_subscription-log.js
+++ migrations/1531948410830_subscription-log.js
@@ -1,5 +1,33 @@
 exports.shorthands = undefined;
 
+// exported so that we can test it
+exports.portIntoLogsQuery = `
+  INSERT INTO subscription_log (firebase_id, topic_id, action, time)
+  SELECT
+    g.data->>'id',
+    g.data->>'topic',
+    g.data->>'action',
+    g.time
+  FROM log_entries_grouped AS g
+  WHERE g.data->>'action' IN ('subscribe', 'unsubscribe');
+`;
+
+exports.portIntoCurrentSubscribersQuery = `
+  INSERT INTO currently_subscribed (firebase_id,topic_id,subscribe_time)
+  SELECT
+    firebase_id, topic_id, MAX(time)
+  FROM subscription_log as s
+  WHERE action = 'subscribe'
+  AND (
+    SELECT COUNT(*) from subscription_log as s2
+    WHERE s2.firebase_id = s.firebase_id
+    AND s2.topic_id = s.topic_id
+    AND s2.action = 'unsubscribe'
+    AND s2.time > s.time
+  ) = 0
+  GROUP BY firebase_id, topic_id;
+`;
+
 exports.up = pgm => {
   pgm.createTable("subscription_log", {
     firebase_id: { type: "varchar", primaryKey: true },
@@ -7,6 +35,10 @@ exports.up = pgm => {
     action: { type: "varchar", notNull: true, primaryKey: true },
     time: { type: "timestamp", notNull: true, primaryKey: true }
   });
+
+  pgm.sql(exports.portIntoLogsQuery);
+  pgm.sql(exports.portIntoCurrentSubscribersQuery);
+
   pgm.createTrigger(
     "currently_subscribed",
     "add_insert_to_log",
